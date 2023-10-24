cd239905210f547fdc3ce06e83c3670c6d2eaf5b
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2023-10-24 03:04:45 +0300

    Add initial support to Entity Framework
    This code was taken from the QuickGrid made by Microsoft
    
    Signed-off-by: Eyal Alon <3756641+iam3yal@users.noreply.github.com>

---
+++ src/RazorSharp.EntityFrameworkAdapter/EntityFrameworkAdapterServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+namespace RazorSharp.EntityFrameworkAdapter;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using RazorSharp.Framework;
+
+/// <summary>
+///     Provides extension methods to configure <see cref="IAsyncQueryExecutor" /> on a <see cref="IServiceCollection" />.
+/// </summary>
+public static class EntityFrameworkAdapterServiceCollectionExtensions
+{
+    /// <summary>
+    ///     Registers an Entity Framework aware implementation of <see cref="IAsyncQueryExecutor" />.
+    /// </summary>
+    /// <param name="services">
+    ///     The <see cref="IServiceCollection" />.
+    /// </param>
+    public static void AddQuickGridEntityFrameworkAdapter(this IServiceCollection services)
+    {
+        services.AddSingleton<IAsyncQueryExecutor, EntityFrameworkAsyncQueryExecutor>();
+    }
+}
---
+++ src/RazorSharp.EntityFrameworkAdapter/EntityFrameworkAsyncQueryExecutor.cs
@@ -0,0 +1,18 @@
+﻿namespace RazorSharp.EntityFrameworkAdapter;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+using RazorSharp.Framework;
+
+internal sealed class EntityFrameworkAsyncQueryExecutor : IAsyncQueryExecutor
+{
+    public Task<int> CountAsync<T>(IQueryable<T> queryable)
+        => queryable.CountAsync();
+
+    public bool IsSupported<T>(IQueryable<T> queryable)
+        => queryable.Provider is IAsyncQueryProvider;
+
+    public Task<T[]> ToArrayAsync<T>(IQueryable<T> queryable)
+        => queryable.ToArrayAsync();
+}
