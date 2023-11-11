bfde25270c7307d5081afd67d97bdcffb6ce9661
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2023-11-11 21:46:08 +0200

    Fix playwright.ps1: trim spaces
    Signed-off-by: Eyal Alon <3756641+iam3yal@users.noreply.github.com>

--- src/playwright.ps1
+++ src/playwright.ps1
@@ -2,4 +2,12 @@ $projectDir = "$PSScriptRoot/RazorSharp.Tests.E2E"
 [xml]$content = Get-Content -Path "$projectDir/RazorSharp.Tests.E2E.csproj"
 $target = $content.Project.PropertyGroup.TargetFramework
 
+if ($target) { 
+    $target = $target | Out-String
+    $target = $target.Trim()
+}
+else {
+    throw "TargetFramework is null or empty."
+}
+
 Start-Process -FilePath "$projectDir/bin/Debug/$target/playwright.ps1" -ArgumentList $args -NoNewWindow
