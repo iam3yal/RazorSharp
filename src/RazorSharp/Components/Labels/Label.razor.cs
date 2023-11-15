cc3fdde69722befcde59b3d27c2bffe683df7053
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2023-11-15 21:05:28 +0200

    Refactor exception logic for unsupported 'Target' types from Razor component to code behind.
    This change includes the replacement of the 'InvalidOperationException' with 'InvalidParameterException'.
    
    Signed-off-by: Eyal Alon <3756641+iam3yal@users.noreply.github.com>

--- src/RazorSharp/Components/Labels/Label.razor.cs
+++ src/RazorSharp/Components/Labels/Label.razor.cs
@@ -1,6 +1,7 @@
 ﻿namespace RazorSharp.Components.Labels;
 
 using RazorSharp.Dom;
+using RazorSharp.Framework;
 
 public sealed partial class Label : WebElementBase
 {
@@ -13,4 +14,14 @@ public sealed partial class Label : WebElementBase
 
     [Parameter]
     public object? Target { get; set; }
+
+    protected override void OnParametersSet()
+    {
+        if (Target is not null and (WebElementBase { Id: null } or not WebElementBase and not string))
+        {
+            throw new InvalidParameterException(
+                nameof(Target),
+                $"Please use a '{typeof(string)}' or a type derived from '{typeof(WebElementBase)}' with a valid '{nameof(Id)}'.");
+        }
+    }
 }
