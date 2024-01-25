146ac3d7bbb8afe213c8afad77c4ce3342e0c0f5
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-01-25 06:04:02 +0200

    Add a note that it's not yet implemented
    Signed-off-by: Eyal Alon <3756641+iam3yal@users.noreply.github.com>

--- src/RazorSharp.Framework/Events/IThrotteable.cs
+++ src/RazorSharp.Framework/Events/IThrotteable.cs
@@ -1,7 +1,10 @@
 namespace RazorSharp.Framework.Events;
 
+using System.Diagnostics.CodeAnalysis;
+
+[SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "NYI")]
 public interface IThrotteable<T>
     where T : class
 {
-    ValueTask ThrottleAsync(Func<T, Task> func, T argument, int interval);
+    ValueTask ThrottleAsync(Func<T, Task> func, T arg, int interval);
 }
